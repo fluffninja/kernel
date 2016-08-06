00edbccb030d306cc28b91b72428d556c43cd7b1
Isaac Polloni <isaacpolloni@outlook.com>
2016-08-06 23:30:10 +0100

    Add stdbool.h and cstdbool

---
+++ include/stdbool.h
@@ -0,0 +1,16 @@
+// stdbool.h for Isaac OS
+
+#ifndef _INC_STDBOOL
+#define _INC_STDBOOL 1
+
+#ifndef __cplusplus
+#define bool    _Bool
+#define true    (1)
+#define false   (0)
+#endif /* __cplusplus */
+
+#define __bool_true_false_are_defined   1
+
+#endif /* _INC_STDBOOL */
+
+
