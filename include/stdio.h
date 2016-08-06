28688443e00595a6eb9f964a482f98fc3de47c59
fluffninja <fluffninja@outlook.com>
2016-08-06 23:20:11 +0100

    Reorder definitions, fix typo, fix FILE definition

--- include/stdio.h
+++ include/stdio.h
@@ -12,7 +12,8 @@ extern "C" {
 
 typedef long fpos_t;
 
-typedef struct {} FILE;
+extern struct __FILE_struct_def;
+typedef struct __FILE_struct_def FILE;
 
 #define BUFSIZ          512
 #define EOF             (-1)
@@ -25,45 +26,25 @@ extern const FILE *stdin;
 extern const FILE *stdout;
 extern const FILE *stderr;
 
-void clearerr(FILE *stream);
-int fclose(FILE *stream);
-int feof(FILE *stream);
-int ferror(FILE *stream);
-int fflush(FILE *stream);
-int fgetc(FILE *stream);
-int fgetpos(FILE *stream, fpos_t *pos);
-char *fgets(char *str, int num, FILE *stream);
-FILE *fopen(const char *filename, const char *mode);
-int fprintf(FILE *stream, const char *format, ...);
-int fputc(int character, FILE *stream);
-int fputs(const char *str, FILE *stream);
-size_t fread(void *ptr, size_t size, size_t count, FILE *stream);
-FILE *reopen(const char *filename, const char *mode, FILE *stream);
-int fscanf(FILE *stream, const char *format, ...);
-int fseek(FILE *stream, long offset, int origin);
-int fsetpos(FILE *stream, const fpos_t *pos);
-long ftell(FILE *stream);
-size_t fwrite(const void *ptr, size_t size, size_t count, FILE *stream);
-int getc(FILE *stream);
-int getchar(void);
-char *gets(char *str);
-void perror(const char *str);
-int printf(const char *format, ...);
-int putc(int character, FILE *stream);
-int putchar(int character);
-int puts(const char *str);
 int remove(const char *filename);
 int rename(const char *oldname, const char *newname);
-void rewind(FILE *stream);
-int scanf(const char *format, ...);
+FILE *tmpfile(void);
+char *tmpnam(char *str);
+
+int fclose(FILE *stream);
+int fflush(FILE *stream);
+FILE *fopen(const char *filename, const char *mode);
+FILE *freopen(const char *filename, const char *mode, FILE *stream);
 void setbuf(FILE *stream, char *buffer);
 int setvbuf(FILE *stream, char *buffer, int mode, size_t size);
+
+int fprintf(FILE *stream, const char *format, ...);
+int fscanf(FILE *stream, const char *format, ...);
+int printf(const char *format, ...);
+int scanf(const char *format, ...);
 int snprintf(char *str, size_t n, const char *format, ...);
 int sprintf(char *str, const char *format, ...);
 int sscanf(const char *str, const char *format, ...);
-FILE *tmpfile(void);
-char *tmpnam(char *str);
-int ungetc(int character, FILE *stream);
 int vfprintf(FILE *stream, const char *format, va_list arg);
 int vfscanf(FILE *stream, const char *format, va_list arg);
 int vprintf(const char *format, va_list arg);
@@ -71,6 +52,32 @@ int vscanf(const char *format, va_list arg);
 int vsnprintf(char *str, size_t n, const char *format, va_list arg);
 int vsprintf(char *str, const char *format, va_list arg);
 int vsscanf(const char *str, const char *format, va_list arg);
+
+int fgetc(FILE *stream);
+char *fgets(char *str, int num, FILE *stream);
+int fputc(int character, FILE *stream);
+int fputs(const char *str, FILE *stream);
+int getc(FILE *stream);
+int getchar(void);
+char *gets(char *str);
+int putc(int character, FILE *stream);
+int putchar(int character);
+int puts(const char *str);
+int ungetc(int character, FILE *stream);
+
+size_t fread(void *ptr, size_t size, size_t count, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t count, FILE *stream);
+
+int fgetpos(FILE *stream, fpos_t *pos);
+int fseek(FILE *stream, long offset, int origin);
+int fsetpos(FILE *stream, const fpos_t *pos);
+long ftell(FILE *stream);
+void rewind(FILE *stream);
+
+void clearerr(FILE *stream);
+int feof(FILE *stream);
+int ferror(FILE *stream);
+void perror(const char *str);
 
 #ifdef __cplusplus
 }
