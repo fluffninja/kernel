65ea17362918c318c5e850d5835000b4dedbd92f
fluffninja <fluffninja@outlook.com>
2016-08-07 00:10:20 +0100

    Add ctype function prototypes

--- include/ctype.h
+++ include/ctype.h
@@ -7,6 +7,21 @@
 extern "C" {
 #endif
 
+int isalnum(int c);
+int isalpha(int c);
+int isblank(int c); // C99/C++11
+int iscntrl(int c);
+int isdigit(int c);
+int isgraph(int c);
+int islower(int c);
+int isupper(int c);
+int isprint(int c);
+int isspace(int c);
+int isxdigit(int c); // Wreck our beautiful alignment, why don't you?
+
+int tolower(int c);
+int toupper(int c);
+
 #ifdef __cplusplus
 }
 #endif
