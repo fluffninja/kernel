a1246c0480dba2fdc689fba0df8952c08ac5731e
fluffninja <fluffninja@outlook.com>
2016-08-06 20:49:09 +0100

    Convert tabs to spaces and fix misalignments courtesy of the contrary

--- include/stdarg.h
+++ include/stdarg.h
@@ -10,10 +10,10 @@ extern "C" {
 typedef unsigned char *va_list;
 
 // Code follows that of www.brokenthorn.com/Resources/OSDev14.html
-#define __VA_SIZE(type)		((sizeof(type) + sizeof(int) - 1) & ~(sizeof(int) - 1))
-#define va_start(ap, arg) 	(ap = ((va_list) &(arg) + __VA_SIZE(arg))
+#define __VA_SIZE(type)     ((sizeof(type) + sizeof(int) - 1) & ~(sizeof(int) - 1))
+#define va_start(ap, arg)   (ap = ((va_list) &(arg) + __VA_SIZE(arg))
 #define va_end(ap)
-#define va_arg(ap, type)	(ap += __VA_SIZE(type), *((type *) (ap - __VA_SIZE(type)))
+#define va_arg(ap, type)    (ap += __VA_SIZE(type), *((type *) (ap - __VA_SIZE(type)))
 
 #ifdef __cplusplus
 }
