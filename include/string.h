31945f05e26facda8d53a2d4fefd3d4f41b0eee1
fluffninja <fluffninja@outlook.com>
2016-08-13 00:50:23 +0100

    Use C standard prototypes Rather than those of the C++ overloads.

--- include/string.h
+++ include/string.h
@@ -9,30 +9,30 @@ extern "C"{
 
 #include <stddef.h>
 
-void *memcpy(void *destination, const void *source, size_t num);
-void *memmove(void *destination, const void *source, size_t num);
-char *strcpy(char *destination, const char *source);
-char *strncpy(char *destination, const char *source, size_t num);
+void *memcpy(void *dest, const void *src, size_t len);
+void *memmove(void *dest, const void *src, size_t len);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t len);
 
-char *strcat(char *destination, const char *source);
-char *strncat(char *destination, const char *source, size_t num);
+char *strcat(char *dest, const char *src);
+char *strncat(char *dest, const char *src, size_t len);
 
-int memcmp(const void *ptr1, const void *ptr2, size_t num);
+int memcmp(const void *ptr1, const void *ptr2, size_t len);
 int strcmp(const char *str1, const char *str2);
 int strcoll(const char *str1, const char *str2);
-int strncmp(const char *str1, const char *str2, size_t num);
-size_t strxfrm(char *destination, const char *source, size_t num);
+int strncmp(const char *str1, const char *str2, size_t len);
+size_t strxfrm(char *dest, const char *src, size_t len);
 
-const void *memchr(const void *str, int value, size_t num);
-const char *strchr(const char *str, int character);
+void *memchr(void *str, int value, size_t len);
+char *strchr(char *str, int c);
 size_t strcspn(const char *str1, const char *str2);
-const char *strpbrk(const char *str1, const char *str2);
-const char *strrchr(const char *str, int character);
+char *strpbrk(char *str1, const char *str2);
+char *strrchr(char *str, int c);
 size_t strspn(const char *str1, const char *str2);
-const char *strstr(const char *str1, const char *str2);
+char *strstr(char *str1, const char *str2);
 char *strtok(char *str, const char *delimiters);
 
-void *memset(void *ptr, int value, size_t num);
+void *memset(void *ptr, int value, size_t len);
 char *strerror(int errnum);
 size_t strlen(const char *str);
 
