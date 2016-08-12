610749e5aece704c8123b23b9fda175f4b4d0b7b
Isaac Polloni <isaacpolloni@outlook.com>
2016-08-13 00:49:42 +0100

    Add least, fast, and (u)intptr types

--- include/stdint.h
+++ include/stdint.h
@@ -3,9 +3,6 @@
 #ifndef _INC_STDINT
 #define _INC_STDINT 1
 
-typedef long long           intmax_t;
-typedef unsigned long long  uintmax_t;
-
 typedef char                int8_t;
 typedef unsigned char       uint8_t;
 typedef short               int16_t;
@@ -15,6 +12,28 @@ typedef unsigned int        uint32_t;
 typedef long long           int64_t;
 typedef unsigned long long  uint64_t;
 
-/* TODO: Add *least* and *fast* types */
+typedef int8_t              int_least8_t;
+typedef uint8_t             uint_least8_t;
+typedef int16_t             int_least16_t;
+typedef uint16_t            uint_least16_t;
+typedef int32_t             int_least32_t;
+typedef uint32_t            uint_least32_t;
+typedef int64_t             int_least64_t;
+typedef uint64_t            uint_least64_t;
+
+typedef int8_t              int_fast8_t;
+typedef uint8_t             uint_fast8_t;
+typedef int16_t             int_fast16_t;
+typedef uint16_t            uint_fast16_t;
+typedef int32_t             int_fast32_t;
+typedef uint32_t            uint_fast32_t;
+typedef int64_t             int_fast64_t;
+typedef uint64_t            uint_fast64_t;
+
+typedef int32_t             intptr_t;
+typedef uint32_t            uintptr_t;
+
+typedef int64_t             intmax_t;
+typedef uint64_t            uintmax_t;
 
 #endif /* _INC_STDINT */
