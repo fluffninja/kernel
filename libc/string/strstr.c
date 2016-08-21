8acd13477582c05c4cae51625113f95741a55ef7
fluffninja <fluffninja@outlook.com>
2016-08-21 23:24:16 +0100

    Implement more string STL

--- libc/string/strstr.c
+++ libc/string/strstr.c
@@ -1,3 +1,19 @@
 #include <string.h>
 
+char *
+strstr(char *str1, const char *str2)
+{
+    if (!str1 || !str2) {
+        return NULL;
+    }
 
+    register size_t i = 0;
+    while (str1[i]) {
+        if (!strcmp(str1 + i, str2)) {
+            break;
+        }
+        ++i;
+    }
+
+    return (str1 + i);
+}
