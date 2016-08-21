a00bfd914ad7834e5c7e7466f1b1d71b6451d837
Isaac Polloni <isaacpolloni@outlook.com>
2016-08-21 23:29:06 +0100

    Add libc start function stub

---
+++ libc/start.c
@@ -0,0 +1,8 @@
+#include <cccompat.h>
+
+void NO_INLINE CDECL
+_start(void) {    
+    // TODO: Call main with argc and argv, then exit (via syscall?), whilst
+    // setting error status.
+    return;
+}
