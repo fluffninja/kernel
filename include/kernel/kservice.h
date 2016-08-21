9c6e083cc5e99d12a9d36f24622568e29937fefb
fluffninja <fluffninja@outlook.com>
2016-08-21 23:17:29 +0100

    Add kernel header stub

---
+++ include/kernel/kservice.h
@@ -0,0 +1,13 @@
+// kservice.h
+// System call interface
+
+#ifndef _INC_KSERVICE
+#define _INC_KSERVICE 1
+
+#include <kernel/khstddef.h>
+
+_KH_CEXTERN_BEGIN
+
+_KH_CEXTERN_END
+
+#endif /* _INC_KSERVICE */
