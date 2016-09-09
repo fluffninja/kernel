a7364b13ecab8f74b2144f8da008e3f2494b03f1
Isaac Polloni <isaacpolloni@outlook.com>
2016-09-09 16:08:48 +0100

    Clean up, add some explanations

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -10,36 +10,27 @@
 #include "vga.h"
 #include "syscall.h"
 
-// Dumb waiting function
-void dumb_wait(int mult)
-{
-    static volatile int a;
-    volatile int *p = &a;
-    for (int i = 0; i < mult * 1000000; ++i) {
-        a = *p;
-        *p = a;
-    }
-}          
-
 void CDECL NO_RETURN
 kmain(void)
 { 
+    // Initialise primary systems
     if (con_init() || 
         idt_init() || 
         isr_init() ||
         syscall_init() || 
-        irq_init() || 
-        ps2_init()) {
+        ps2_init() || 
+        irq_init()) {
+        // If any of these fail, we cannot continue
         panic("init error\n");
     }
 
+    // Turn on fullscreen panic
     panic_set_use_bsod(1);
 
+    // Enable interrupts
     sti();
 
-    // Non-essential drivers (whose initialisation upon which we don't depend)
     kb_init();
-    //mouse_init();
     vga_init();
 
     while (1);
