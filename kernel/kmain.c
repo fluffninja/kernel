61852f9fd140ef845ff1eedc9f5d6e8380d407d9
fluffninja <fluffninja@outlook.com>
2016-10-21 00:00:08 +0100

    Reorganise and explain early init routine

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -14,28 +14,47 @@
 #include "vga.h"
 #include "syscall.h"
 
-void call_test_fn(void);
-
 void CDECL NO_RETURN
 kmain(void)
 {
+    // Get the boot parameter block left by the bootloader
     struct kernel_boot_params *params = boot_get_params(BOOT_PARAM_BLOCK_ADDR);
 
-    // Initialise primary systems
-    if (con_init(params) ||
-        idt_init() ||
-        isr_init() ||
-        syscall_init() ||
-        ps2_init() ||
-        irq_init()) {
-        // If any of these fail, we cannot continue
-        panic("init error\n");
+    // Initialise the console so that we've got some kind of output
+    if (con_init(params)) {
+        // If we cannot initialise the console - awkward.
+        // There's not a lot we can do about this, so panic.
+        panic("init error: console problem\n");
     }
 
-    // Enable interrupts
+    // Initialise the CPU - that is: the IDT and exception ISRs
+    if (idt_init() || isr_init()) {
+        panic("init error: cpu problem\n");
+    }
+
+    // Initialise hardware things.
+    // We initialise the PS2 controller first, so that we can tell
+    // it not to spam us with IRQs before we've gotten our IRQ management
+    // sorted out.
+    if (ps2_init() || irq_init()) {
+        panic("init error: hardware interrupt problem\n");
+    }
+
+    // Initiliase system call interface
+    if (syscall_init()) {
+        panic("init error: syscall problem\n");
+    }
+
+    // We're ready to accept interrupts now
     sti();
 
+    // From now on, we don't need to panic if a component fails to load.
+
+    // Keyboard
     kb_init();
+
+    // Basic VGA driver - we assume that VGA functions are safely usable
+    // after this.
     vga_init();
 
     // Enable VGA cursor by setting shape
