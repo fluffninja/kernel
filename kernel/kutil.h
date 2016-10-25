230e1caf34ce3b71652b61f87d1cbc861b533d6a
Isaac Polloni <isaacpolloni@outlook.com>
2016-10-25 19:05:29 +0100

    Fix formatting peculiarity

--- kernel/kutil.h
+++ kernel/kutil.h
@@ -13,14 +13,15 @@
 #define ARRLEN(A)       (sizeof(A) / sizeof(*(A)))
 #endif
 
-INLINE void *
-kzeromem(void *ptr, size_t sz)
 // Casts a type's member to the containing type
 // ITEM_PTR         - pointer to a member
 // CONTAINER_TYPE   - type of the containing type
 // MEMBER           - name of the member within the type
 #define CONTAINER_OF(ITEM_PTR, CONTAINER_TYPE, MEMBER) \
     (CONTAINER_TYPE *) ((char *) ITEM_PTR - offsetof(CONTAINER_TYPE, MEMBER)
+
+// Fill the region of memory pointed to by 'ptr' of size 'sz' with zeroes.
+INLINE void *kzeromem(void *ptr, size_t sz)
 {
     return kmemset(ptr, sz, 0);
 }
