303698729dd625b49df348bcced5fe698b0b79e1
fluffninja <fluffninja@outlook.com>
2016-10-25 19:08:54 +0100

    Add 'smart' kzeromem
    If enabled (via the SMART_KZEROMEM define), the kzeromem will
    now zero memory in 4-, 2-, or 1-byte blocks if the size of the
    region to zero is a multiple of 4, 2, or 1 respectively.

--- kernel/kutil.h
+++ kernel/kutil.h
@@ -20,10 +20,23 @@
 #define CONTAINER_OF(ITEM_PTR, CONTAINER_TYPE, MEMBER) \
     (CONTAINER_TYPE *) ((char *) ITEM_PTR - offsetof(CONTAINER_TYPE, MEMBER)
 
+#define SMART_KZEROMEM 1
+
 // Fill the region of memory pointed to by 'ptr' of size 'sz' with zeroes.
 INLINE void *kzeromem(void *ptr, size_t sz)
 {
+    #ifdef SMART_KZEROMEM
+    // The compiler _should_ be clever enough to optimise this out
+    if (sz % 4 == 0) {
+        return kmemset32(ptr, sz, 0);
+    } else if (sz % 2 == 0) {
+        return kmemset16(ptr, sz, 0);
+    } else {
+        return kmemset(ptr, sz, 0);
+    }
+    #else
     return kmemset(ptr, sz, 0);
+    #endif /* SMARK_KZEROMEM */
 }
 
 #endif /* _INC_KUTIL */
