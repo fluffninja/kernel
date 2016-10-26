43b3281b53a64064c965cc6890575cf9683fe45c
Isaac Polloni <isaacpolloni@outlook.com>
2016-10-26 01:17:47 +0100

    Add dlist for-each iteration helper
    Example Usage:
    struct foo { int val; struct dlist_node list; } f1, f2, f3, ...;
    //...
    DLIST_FOR_EACH(struct foo, f, &f1, list) {
    	printf("Val: %d\n", f->val);
    }

--- include/kernel/dlist.h
+++ include/kernel/dlist.h
@@ -75,4 +75,40 @@ static INLINE void dlist_split_after(struct dlist_node *new,
     __dlist_split(node, new);
 }
 
+// Casts a type's member to the containing type
+// ITEM_PTR:    - pointer to a member
+// CONT_TYPE:   - type of the containing type
+// MEMBER:      - name of the member within the type
+#ifndef CONTAINER_OF
+#define CONTAINER_OF(ITEM_PTR, CONT_TYPE, MEMBER) \
+    (CONT_TYPE *) ((char *) (ITEM_PTR) - offsetof(CONT_TYPE, MEMBER))
+#endif
+
+#define DLIST_CONTAINER_OF_NEXT(NODE, CONT_TYPE, MEMBER) \
+    CONTAINER_OF(NODE->MEMBER.next, CONT_TYPE, MEMBER)
+
+#define DLIST_CONTAINER_OF_PREV(NODE, CONT_TYPE, MEMBER) \
+    CONTAINER_OF(NODE->MEMBER.prev, CONT_TYPE, MEMBER)
+
+// Iterates forwards through a given list.
+// CONT_TYPE:   The type of the given container.
+// ITER:        The name of the iterator variable within the loop body.
+// CONT:        The first container.
+// MEMBER:      Name of the list node as a member of container CONT.
+//
+// Example Usage:
+// struct foo { int val; struct dlist_node list; } f1, f2, f3, ...;
+// ...
+// DLIST_FOR_EACH(struct foo, f, &f1, list) {
+//     printf("Val: %d\n", f->val);
+// }
+#define DLIST_FOR_EACH(CONT_TYPE, ITER, CONT, MEMBER)               \
+    for (                                                           \
+        CONT_TYPE *ITER = CONT,                                     \
+        *__first = NULL;                                            \
+        ITER != __first;                                            \
+        __first = !__first ? ITER : __first,                        \
+        ITER = DLIST_CONTAINER_OF_NEXT(ITER, CONT_TYPE, MEMBER)     \
+    )
+
 #endif /* _INC_DLIST */
