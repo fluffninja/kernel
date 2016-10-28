0a2df8cfe1bd061843abb56d6f19248f53c25ce6
Isaac Polloni <isaacpolloni@outlook.com>
2016-10-28 13:59:12 +0100

    Add max_align_t

--- include/stddef.h
+++ include/stddef.h
@@ -3,14 +3,12 @@
 #ifndef _INC_STDDEF
 #define _INC_STDDEF 1
 
-#define NULL    ((void *) 0)
+#define NULL                    ((void *) 0)
 
 #define offsetof(type, member)  ((unsigned int) &(((type *) 0)->member))
 
-typedef unsigned int    size_t;
-typedef int             ptrdiff_t;
-
-// Currently omitting max_align_t from C11.
-// nullptr_t is C++ only.
+typedef unsigned int            size_t;
+typedef int                     ptrdiff_t;
+typedef long double             max_align_t;
 
 #endif /* _INC_STDDEF */
