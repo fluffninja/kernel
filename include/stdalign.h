2ac3cbbfbe93849c55521b71c33e6e565ad05e90
fluffninja <fluffninja@outlook.com>
2016-10-28 14:03:14 +0100

    Add stdalign.h

---
+++ include/stdalign.h
@@ -0,0 +1,14 @@
+// stdalign.h
+
+#ifndef _INC_STDALIGN
+#define _INC_STDALIGN 1
+
+#ifndef __cplusplus
+    #define alignas _Alignas
+    #define alignof _Alignof
+#endif
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif /* _INC_STDALIGN */
