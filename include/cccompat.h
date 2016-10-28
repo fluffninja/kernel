f5f89de7173f562ec1fc80e23d1cb16a57f49c4c
Isaac Polloni <isaacpolloni@outlook.com>
2016-10-28 14:02:46 +0100

    Use gcc attribute special names
    Append and prepend gcc attribute names with double underscores,
    so that they cannot accidentally become defined as something
    else - such as noreturn from C11's stdnoreturn (adding soon).

--- include/cccompat.h
+++ include/cccompat.h
@@ -13,20 +13,20 @@
 
 #ifdef _COMP_GCC
     #define INLINE          inline
-    #define ALWAYS_INLINE   INLINE __attribute__((always_inline))
-    #define NAKED           __attribute__((naked))
-    #define NO_INLINE       __attribute__((noinline))
-    #define NO_RETURN       __attribute__((noreturn))
-    #define NO_THROW        __attribute__((nothrow))
-    #define NO_OPTIMISE     __attribute__((optimize("O0")))
+    #define ALWAYS_INLINE   inline __attribute__((__always_inline__))
+    #define NAKED           __attribute__((__naked__))
+    #define NO_INLINE       __attribute__((__noinline__))
+    #define NO_RETURN       __attribute__((__noreturn__))
+    #define NO_THROW        __attribute__((__nothrow__))
+    #define NO_OPTIMISE     __attribute__((__optimize__("O0")))
     #define CDECL           __attribute__((__cdecl__))
-    #define ALIGN(A)        __attribute__((aligned(A)))
+    #define ALIGN(A)        __attribute__((__aligned__(A)))
     #define BEGIN_PACK
-    #define END_PACK        __attribute__((packed))
+    #define END_PACK        __attribute__((__packed__))
     #define ASM             __asm__
     #define ASM_VOLATILE    __asm__ volatile
     #define ASM_GOTO        __asm__ goto
-    #define NO_REMOVE       __attribute__((used))
+    #define NO_REMOVE       __attribute__((__used__))
 #else
     #define INLINE
     #define ALWAYS_INLINE
@@ -42,6 +42,7 @@
     #define ASM
     #define ASM_VOLATILE
     #define ASM_GOTO
+    #define NO_REMOVE
 #endif
 
 #endif /* _INC_CCCOMPAT */
