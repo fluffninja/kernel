dcfbb329b575260816f9cc0dbe1e78a9de25f622
fluffninja <fluffninja@outlook.com>
2016-10-28 14:03:24 +0100

    Improve formatting

--- include/stdbool.h
+++ include/stdbool.h
@@ -4,11 +4,11 @@
 #define _INC_STDBOOL 1
 
 #ifndef __cplusplus
-#define bool    _Bool
-#define true    (1)
-#define false   (0)
-#endif /* __cplusplus */
+    #define bool    _Bool
+    #define true    (1)
+    #define false   (0)
+#endif
 
-#define __bool_true_false_are_defined   1
+#define __bool_true_false_are_defined 1
 
 #endif /* _INC_STDBOOL */
