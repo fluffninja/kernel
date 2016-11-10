322f5b77de0618a2a0a6b3774c2dfd39e419feae
Isaac Polloni <isaacpolloni@outlook.com>
2016-11-10 20:15:12 +0000

    Add STL iso646.h and C++ counterpart

---
+++ include/iso646.h
@@ -0,0 +1,23 @@
+// iso646.h for IsaacOS
+
+#ifndef _INC_ISO646
+#define _INC_ISO646 1
+
+// Logic operations
+#define and         &&
+#define or          ||
+#define not         !
+
+// Bitwise operations
+#define bitand      &
+#define bitor       |
+#define xor         ^
+#define compl       ~
+
+// Assignment operations
+#define and_eq      &=
+#define or_eq       |=
+#define not_eq      !=
+#define xor_eq      ^=
+
+#endif /* _INC_ISO646 */
