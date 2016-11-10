e89e9292bb003a7a76cd1dc76f38d32352b59377
fluffninja <fluffninja@outlook.com>
2016-11-10 20:12:02 +0000

    Rework panic flags

--- kernel/panic.h
+++ kernel/panic.h
@@ -4,13 +4,17 @@
 #include <kernel/asm/cpustat.h>
 
 enum {
-    PANIC_SHOW_DUMP         = 0x0001,
-    PANIC_CLEAR_SCREEN      = 0x0002,
-    PANIC_USE_COLOUR        = 0x0004,
-    PANIC_SHOW_HELP         = 0x0008,
-    PANIC_USE_BSOD          = PANIC_CLEAR_SCREEN
-                            | PANIC_USE_COLOUR
-                            | PANIC_SHOW_HELP,
+    PANIC_CLEAR             = 0x0001,
+    PANIC_COLOUR            = 0x0002,
+    PANIC_HELP_TEXT         = 0x0004,
+    PANIC_DUMP_CPU          = 0x0008,
+    PANIC_DUMP_STACK        = 0x0010,
+    PANIC_FULL_DUMP         = PANIC_DUMP_STACK
+                            | PANIC_DUMP_CPU,
+    PANIC_BSOD              = PANIC_CLEAR
+                            | PANIC_COLOUR
+                            | PANIC_HELP_TEXT
+                            | PANIC_FULL_DUMP,
 };
 
 uint32_t panic_set_flags(uint32_t flags, int state);
