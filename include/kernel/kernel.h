a9db9ec347b4b9fef9afb5e5b8644ab6370afb13
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-27 15:52:42 +0000

    Move kutil.h macros to kernel.h public header

--- include/kernel/kernel.h
+++ include/kernel/kernel.h
@@ -10,6 +10,28 @@
 extern "C" {
 #endif
 
+#define PAGE_SIZE       4096
+#define PAGE_ALIGNED    ALIGN(PAGE_SIZE)
+
+#ifndef ARRLEN
+#define ARRLEN(A)       (sizeof(A) / sizeof(*(A)))
+#endif
+
+// CONTAINER_OF:
+//   Cast a pointer to a member within a container to a pointer to the
+//   container of the container type.
+// MEMBER_POINTER   - pointer to a member.
+// CONTAINER_TYPE   - type of the container to which the member belongs.
+// MEMBER_NAME      - name of the member field within the container type.
+#ifndef CONTAINER_OF
+#define CONTAINER_OF(MEMBER_POINTER, CONTAINER_TYPE, MEMBER_NAME)             \
+    (                                                                         \
+        (CONTAINER_TYPE *) (                                                  \
+            (char *) (MEMBER_POINTER) - offsetof(CONTAINER_TYPE, MEMBER_NAME) \
+        )                                                                     \
+    )
+#endif
+
 static INLINE void *kmemset32(void *pointer, size_t size, uint32_t val)
 {
     uint32_t *pointer32 = (uint32_t *) pointer;
@@ -94,6 +116,10 @@ static INLINE char *kstrcpy(const char *source, char *dest)
 
     return dest;
 }
+
+#ifndef KZEROMEM
+#define KZEROMEM(POINTER, SIZE) kmemset((POINTER), (SIZE), 0)
+#endif
 
 #ifdef __cplusplus
 }
