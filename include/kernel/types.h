e412f2f641d9db84c4152de3b5130bafc41da2fe
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-28 13:42:32 +0000

    Add standard kernel types public header
    Here we introduce shorthand integer types (to reduce
    dependency on stdint within the kernel) and the
    prospect of using C's _Bool (as bool) type (rather than
    integers) to represent boolean values. This should make
    the purpose of boolean-dealing code far clearer.

---
+++ include/kernel/types.h
@@ -0,0 +1,19 @@
+#ifndef _INC_KERNEL_TYPES
+#define _INC_KERNEL_TYPES 1
+
+typedef char                s8;
+typedef unsigned char       u8;
+typedef short               s16;
+typedef unsigned short      u16;
+typedef int                 s32;
+typedef unsigned int        u32;
+typedef long long           s64;
+typedef unsigned long long  u64;
+
+#ifndef __cplusplus
+    #define bool    _Bool
+    #define true    (1)
+    #define false   (0)
+#endif
+
+#endif /* _INC_KERNEL_TYPES */
