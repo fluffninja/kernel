cb936bed9c5a067edad20d3acea18ed448fffd80
fluffninja <fluffninja@outlook.com>
2017-01-28 14:30:23 +0000

    Rename kernel error header to kerror

--- include/kernel/kerror.h
+++ include/kernel/kerror.h
@@ -3,12 +3,15 @@
 
 enum {
     // Success
-    KERROR_OK = 0,
+    KERROR_NONE = 0,
 
     // Failcode
     KERROR_ARG_NULL,
     KERROR_ARG_OUT_OF_RANGE,
     KERROR_ARG_INVALID,
+
+    // Must always be last
+    KERROR_LAST,
 };
 
 #endif /* _INC_KERNEL_ERROR */
