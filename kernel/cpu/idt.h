c953083dc1133b4e7d0fba5776d2bf646a03ac12
fluffninja <fluffninja@outlook.com>
2017-01-28 14:51:57 +0000

    Tidy up IDT interface and implementation
    * Utilise kernel error codes.
    * Utilise kernel types (integer types and bool).
    * Privatise the idt_entry and idt_descriptor types - they aren't
    required by the interface.
    
    This pattern is to be applied to other private kernel sources.1

--- kernel/cpu/idt.h
+++ kernel/cpu/idt.h
@@ -1,9 +1,9 @@
 #ifndef _INC_IDT
 #define _INC_IDT 1
 
-#include <stdint.h>
+#include <kernel/types.h>
 
-#include <kernel/compiler.h>
+#define IDT_MAX 256
 
 // IDT Entry Flags
 #define IDT_PRESENT             0x80 // Interrupt entry should be used
@@ -17,32 +17,9 @@
 #define IDT_GATE_INTERRUPT_32   0x0e // This is a 32-bit interrupt gate
 #define IDT_GATE_TRAP_32        0x0f // This is a 32-bit trap gate
 
-BEGIN_PACK struct idt_entry {
-    uint16_t handler_offset_00_15;  // Offset of handler code in GDT segment
-    uint16_t gdt_handler_selector;  // GDT segment - as table selector
-    uint8_t  _reserved0;            // Reserved byte
-    uint8_t  flags;                 // Flags detailing type and attributes
-    uint16_t handler_offset_16_32;  // Upper word of handler offset
-} END_PACK;
-
-#define IDT_DEF_ENTRY(HANDLER_OFFSET, GDT_SELECTOR, FLAGS)      \
-    {                                                           \
-        (uint16_t) (((uint32_t) (HANDLER_OFFSET)) & 0xffff),    \
-        (uint16_t) (GDT_SELECTOR),                              \
-        0,                                                      \
-        (uint8_t) (FLAGS),                                      \
-        (uint16_t) (((uint32_t) (HANDLER_OFFSET)) >> 16),       \
-    }
-
-// 48-bit IDT descriptor, for use with lidt and sidt
-BEGIN_PACK struct idt_descriptor {
-    uint16_t            size;
-    struct idt_entry    *base;
-} END_PACK;
-
 int idt_init(void);
-int idt_is_valid_index(int index);
-int idt_has_entry(int index);
+bool idt_is_valid_index(int index);
+bool idt_has_entry(int index);
 int idt_set_entry(int index, void (*handler)(void), int selector, int flags);
 
 #endif /* _INC_IDT */
