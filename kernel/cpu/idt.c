18f4f900ec311e30644e61b9f1108761554c9ee2
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-28 14:51:57 +0000

    Tidy up IDT interface and implementation
    * Utilise kernel error codes.
    * Utilise kernel types (integer types and bool).
    * Privatise the idt_entry and idt_descriptor types - they aren't
    required by the interface.
    
    This pattern is to be applied to other private kernel sources.1

--- kernel/cpu/idt.c
+++ kernel/cpu/idt.c
@@ -1,53 +1,105 @@
 #include <kernel/kernel.h>
+#include <kernel/compiler.h>
+#include <kernel/types.h>
 #include <kernel/klog.h>
+#include <kernel/kerror.h>
 
 #include "idt.h"
 
-#define IDT_SIZE 256
-
-static struct idt_entry s_idt[IDT_SIZE];
-
-static INLINE int __idt_is_valid_index_impl(int index)
-{
-    return !((index < 0) || (index >= (int) ARRLEN(s_idt)));
+BEGIN_PACK
+struct idt_entry {
+    u16 handler_offset_low_word;    // Low word of handler offset within the
+                                    // given GDT segment.
+    u16 gdt_handler_selector;       // GDT selector for the GDT segment
+                                    // containing the given handler.
+    u8 reserved;
+    u8 flags;                       // Flags for this IDT entry.
+    u16 handler_offset_high_word;   // High word of handler offset.
 }
+END_PACK;
 
-int idt_init(void)
+BEGIN_PACK
+struct idt_descriptor {
+    u16 size;                   // Number of entries in the IDT.
+    struct idt_entry *base;     // Address of the first entry of the IDT.
+}
+END_PACK;
+
+static struct idt_entry s_idt[IDT_MAX];
+
+static int initialsie_entry(struct idt_entry *entry, void *handler_offset,
+    int gdt_selector, int flags)
 {
-    KZEROMEM(s_idt, sizeof(s_idt));
+    if (!entry) {
+        return KERROR_ARG_NULL;
+    }
 
-    struct idt_descriptor descriptor = { sizeof(s_idt), s_idt };
+    KZEROMEM(entry, sizeof(struct idt_entry));
 
-    ASM_VOLATILE("lidt [eax]"::"a"(&descriptor));
-
-    klog_printf("idt: loaded with size %d at %p\n", ARRLEN(s_idt), s_idt);
+    entry->handler_offset_low_word = (u16) (((int) handler_offset) & 0xffff);
+    entry->handler_offset_high_word = (u16) (((int) handler_offset) >> 16);
+    entry->gdt_handler_selector = (u16) gdt_selector;
+    entry->flags = (u8) flags;
 
     return 0;
 }
 
-int idt_is_valid_index(int index)
+static void load_descriptor(int size, void *base)
 {
-    return __idt_is_valid_index_impl(index);
+    struct idt_descriptor descriptor;
+    descriptor.size = size;
+    descriptor.base = base;
+    ASM_VOLATILE(
+        "lidt [eax]"::
+        "a"(&descriptor)
+    );
 }
 
-int idt_has_entry(int index)
+int idt_init(void)
 {
-    if (!__idt_is_valid_index_impl(index)) {
-        return 1;
+    // Zero out the IDT
+    KZEROMEM(s_idt, sizeof(s_idt));
+
+    load_descriptor((int) sizeof(s_idt), s_idt);
+
+    klog_printf("idt: loaded at %p with %d entries (%zuB)\n", s_idt,
+        ARRLEN(s_idt), sizeof(s_idt));
+
+    return 0;
+}
+
+static INLINE bool _is_valid_index(int index)
+{
+    return ((index >= 0) && (index <= IDT_MAX));
+}
+
+bool idt_is_valid_index(int index)
+{
+    return _is_valid_index(index);
+}
+
+bool idt_has_entry(int index)
+{
+    if (!_is_valid_index(index)) {
+        return false;
     }
 
     struct idt_entry entry = s_idt[index];
-    return (entry.flags & IDT_PRESENT);
+
+    return ((bool) (entry.flags & IDT_PRESENT));
 }
 
 int idt_set_entry(int index, void (*handler)(void), int selector, int flags)
 {
-    if (!__idt_is_valid_index_impl(index)) {
-        return 1;
+    if (!_is_valid_index(index)) {
+        return KERROR_ARG_OUT_OF_RANGE;
     }
 
-    struct idt_entry entry = IDT_DEF_ENTRY(handler, selector, flags);
-    s_idt[index] = entry;
+    struct idt_entry *entry_ptr = &(s_idt[index]);
+
+    if (initialsie_entry(entry_ptr, (void *) handler, selector, flags)) {
+        return KERROR_UNSPECIFIED;
+    }
 
     return 0;
 }
