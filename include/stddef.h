fcfdfcafc7e1b5d9498cc92a7d8677076088f83d
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-28 13:39:28 +0000

    Undef NULL if it is already defined before stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -3,12 +3,18 @@
 #ifndef _INC_STDDEF
 #define _INC_STDDEF 1
 
-#define NULL                    ((void *) 0)
+// We want there to be one official definition of NULL for a given compilation
+// unit that includes stddef.h.
+#ifdef NULL
+    #undef NULL
+#endif
 
-#define offsetof(type, member)  ((unsigned int) &(((type *) 0)->member))
+#define NULL ((void *) 0)
 
-typedef unsigned int            size_t;
-typedef int                     ptrdiff_t;
-typedef long double             max_align_t;
+#define offsetof(type, member) ((unsigned int) &(((type *) 0)->member))
+
+typedef unsigned int    size_t;
+typedef int             ptrdiff_t;
+typedef long double     max_align_t;
 
 #endif /* _INC_STDDEF */
