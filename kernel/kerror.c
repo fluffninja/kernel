9f47214f1773774b4de6109b85d0c1a837b1a2a0
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-28 15:21:09 +0000

    Rename kerror function get to pop, add peek

--- kernel/kerror.c
+++ kernel/kerror.c
@@ -2,19 +2,26 @@
 
 static int s_error = KERROR_NONE;
 
-void kerror_clear(void)
-{
-    s_error = KERROR_NONE;
-}
-
-void kerror_set(int error)
+void kerror_set_last(int error)
 {
     if ((error >= 0) && (error < KERROR_LAST)) {
         s_error = error;
     }
 }
 
-int kerror_get_last(void)
+void kerror_clear_last(void)
+{
+    s_error = 0;
+}
+
+int kerror_pop_last(void)
+{
+    int error = s_error;
+    s_error = 0;
+    return error;
+}
+
+int kerror_peek_last(void)
 {
     return s_error;
 }
