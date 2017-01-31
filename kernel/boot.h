e932f8e106774d5948af335595a22a750d1415ee
Isaac Polloni <isaacpolloni@outlook.com>
2017-01-31 00:27:03 +0000

    Remove stddef and stdint dependency

--- kernel/boot.h
+++ kernel/boot.h
@@ -1,22 +1,22 @@
 #ifndef _INC_BOOT
 #define _INC_BOOT 1
 
-#include <stdint.h>
-
+#include <kernel/kernel.h>
+#include <kernel/types.h>
 #include <kernel/compiler.h>
 
 // The kernel boot parameter block allows the bootloader to hand-over binary
 // information to the kernel during the boot process. Its presence and validity
 // are confirmed via a signature.
 BEGIN_PACK struct kernel_boot_params {
-    uint16_t    signature;
-    uint8_t     cursor_x;
-    uint8_t     cursor_y;
+    u16 signature;
+    u8  cursor_x;
+    u8  cursor_y;
 } END_PACK;
 
 // These values MUST agree with the values in boot/boot.asm
-#define BOOT_PARAM_BLOCK_ADDR   0x7000
-#define BOOT_PARAM_BLOCK_SIG    0xc33c
+#define BOOT_PARAM_BLOCK_ADDR   ((void *) 0x00007000)
+#define BOOT_PARAM_BLOCK_SIG    0x0000c33c
 
 // Acquires the kernel boot parameter block from its location in memory.
 // addr:
@@ -24,6 +24,6 @@ BEGIN_PACK struct kernel_boot_params {
 // Return:
 // If the given address does not contain a valid parameter block, then a null
 // pointer is returned.
-struct kernel_boot_params *boot_get_params(intptr_t addr);
+struct kernel_boot_params *boot_get_params(void *addr);
 
 #endif /* _INC_BOOT */
