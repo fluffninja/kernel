60e496c69551fc66e44d0e36608c439b3e2f3e03
fluffninja <fluffninja@outlook.com>
2021-07-12 02:10:36 +0100

    Use gcc builtins for stdarg definitons

--- include/stdarg.h
+++ include/stdarg.h
@@ -3,12 +3,10 @@
 #ifndef _INC_STDARG
 #define _INC_STDARG 1
 
-typedef unsigned char *va_list;
+typedef __builtin_va_list va_list;
 
-#define _VA_SIZEOF(t)   ((sizeof(t) + sizeof(int) - 1) & ~(sizeof(int) - 1))
-
-#define va_start(ap, v) ((ap) = (va_list) &(v) + _VA_SIZEOF(v))
-#define va_end(ap)      ((ap) = (va_list) 0)
-#define va_arg(ap, t)   (*(t *) (((ap) += _VA_SIZEOF(t)) - _VA_SIZEOF(t)))
+#define va_start(ap, v) __builtin_va_start(ap, v)
+#define va_end(ap) __builtin_va_end(ap)
+#define va_arg(ap, t) __builtin_va_arg(ap, t)
 
 #endif /* _INC_STDARG */
